19654051dcf6a0834c5c9f8dc362d6891bb6605d
Oleh Volkolupov <olegvolkolupov@gmail.com>
2021-11-30 21:34:12 +0100

    ^add_task-02a^

--- task-02.js
+++ task-02.js
@@ -4,24 +4,24 @@ const TOTAL = 100;
 // количество единиц товара в заказе
 let ordered = 50;
 if(ordered > TOTAL)
-    console.log("На складе недостаточно твоаров!");
+    console.log("На складе недостаточно товаров!");
 else
     console.log("Заказ оформлен, с вами свяжется менеджер");
 
 ordered = 20;
 if(ordered > TOTAL)
-    console.log("На складе недостаточно твоаров!");
+    console.log("На складе недостаточно товаров!");
 else
     console.log("Заказ оформлен, с вами свяжется менеджер");
 
 ordered = 80;
 if(ordered > TOTAL)
-    console.log("На складе недостаточно твоаров!");
+    console.log("На складе недостаточно товаров!");
 else
     console.log("Заказ оформлен, с вами свяжется менеджер");
 
 ordered = 130;
 if(ordered > TOTAL)
-    console.log("На складе недостаточно твоаров!");
+    console.log("На складе недостаточно товаров!");
 else
     console.log("Заказ оформлен, с вами свяжется менеджер");
